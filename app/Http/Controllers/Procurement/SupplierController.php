2234da97f56edb7333ef8b276df8fbc1c69f4fe3
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2026-09-03 18:35:44 +0300

    Navi fix

--- app/Http/Controllers/Procurement/SupplierController.php
+++ app/Http/Controllers/Procurement/SupplierController.php
@@ -157,7 +157,7 @@ class SupplierController extends Controller
             ],
             'is_vat_registered' => 'sometimes|boolean',
             'vat_number' => 'nullable|string|max:50|required_if:is_vat_registered,1',
-            'withholding_tax_applicable' => 'sometimes|boolean',
+            'withholding_tax_applicable' => 'required|boolean',
             'withholding_tax_rate' => 'nullable|numeric|min:0|max:100|required_if:withholding_tax_applicable,1',
             'withholding_tax_exemption_ref' => 'nullable|string|max:100',
             'withholding_tax_exemption_expiry' => 'nullable|date|after:today',
